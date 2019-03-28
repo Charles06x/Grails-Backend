b54eaa7875db905d9f3d9cd1df6d1f2819de4f56
Charles <chad9591@gmail.com>
2019-03-27 22:54:52 -0500

    Initial commid, grails project with mysql database and domain class 'user'

---
+++ grails-app/domain/grails/backend/User.groovy
@@ -0,0 +1,11 @@
+package grails.backend
+
+class User {
+    String user_name
+    String pwd
+    String user_nick
+    Int rol_idrol
+
+    static constraints = {
+    }
+}
---
+++ grails-app/init/grails/backend/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails.backend
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}

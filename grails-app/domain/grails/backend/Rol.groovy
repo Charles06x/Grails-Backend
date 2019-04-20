90b4a7f1120bc276107887539622c623290b5451
Charles <chad9591@gmail.com>
2019-04-19 21:05:06 -0500

    Convert domains into rest resources, and allow publicKeyRetrieval.

--- grails-app/domain/grails/backend/Rol.groovy
+++ grails-app/domain/grails/backend/Rol.groovy
@@ -1,5 +1,8 @@
 package grails.backend
 
+import grails.rest.*
+
+@Resource(uri='/Rols', formats=['json', 'xml']) //Gives priority to json format for the response.
 class Rol {
 
     String rolName
--- grails-app/domain/grails/backend/User.groovy
+++ grails-app/domain/grails/backend/User.groovy
@@ -1,5 +1,8 @@
 package grails.backend
 
+import grails.rest.*
+
+@Resource(uri='/Users', formats=['json', 'xml']) //Gives priority to json format for the response.
 class User {
     
     String name

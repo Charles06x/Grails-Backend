c8db71b388517141338e4e4c61742872c6095b7d
Charles <chad9591@gmail.com>
2019-05-08 20:32:05 -0500

    Revert the authentication.

--- grails-app/domain/grails/backend/User.groovy
+++ grails-app/domain/grails/backend/User.groovy
@@ -1,26 +1,11 @@
 package grails.backend
 
-import groovy.transform.EqualsAndHashCode
-import groovy.transform.ToString
-import grails.compiler.GrailsCompileStatic
-
-@GrailsCompileStatic
-@EqualsAndHashCode(includes='username')
-@ToString(includes='username', includeNames=true, includePackage=false)
-class User implements Serializable {
-
-    private static final long serialVersionUID = 1
-
-    String username
-    String password
-    boolean enabled = true
-    boolean accountExpired
-    boolean accountLocked
-    boolean passwordExpired
-
-    Set<Authority> getAuthorities() {
-        (UserAuthority.findAllByUser(this) as List<UserAuthority>)*.authority as Set<Authority>
-    }
+class User {
+    
+    String name
+    String pwd
+    String code
+    Rol rol
 
     static constraints = {
         password nullable: false, blank: false, password: true
